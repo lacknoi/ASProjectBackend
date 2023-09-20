305b699a9afdcc27f9bb3221bed5855a5bd91982
CORP-AIS900\noppholn <noppholn@ais.co.th>
2023-09-20 20:37:31 +0700

    add api gateway service

---
+++ api-gateway/src/main/java/com/improve/skill/as/apigatewayservice/APIGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.improve.skill.as.apigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APIGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APIGatewayServiceApplication.class, args);
+	}
+
+}
